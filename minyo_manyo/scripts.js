fa2cd795b8d5e3d0295b9b4ce9d11b1bbc4b87d8
Spawn <spawn@192.168.1.9>
2024-02-12 20:11:53 +0100

    wow, rounded corners such webdev

---
+++ minyo_manyo/scripts.js
@@ -0,0 +1,7 @@
+const basket = document.getElementById("basket");
+const sidebar = document.getElementById("sidebar");
+const products = document.getElementById("products");
+
+function main() {
+    // does stuff on startup
+}
