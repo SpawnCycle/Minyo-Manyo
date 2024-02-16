dc4c8d1a02c12e51f3114908548b237be159cd80
BDonat05 <bujakidonat@gmail.com>
2024-02-16 15:52:32 +0100

    ennyire futja péntek délután css

--- minyo_manyo/scripts.js
+++ minyo_manyo/scripts.js
@@ -1,7 +1,27 @@
 const basket = document.getElementById("basket");
-const sidebar = document.getElementById("sidebar");
+const sidebar = document.getElementById("sidebar-div");
 const products = document.getElementById("products");
 
 function main() {
     // does stuff on startup
+}
+
+let isBasketVisible = false;
+function toggleBasket() {
+    if (isBasketVisible == false) {
+        basket.style.display = "inline";
+    } else {
+        basket.style.display = "none";
+    }
+    isBasketVisible = !isBasketVisible;
+}
+
+let isSidebarVisible = false;
+function toggleSidebar() {
+    if (isSidebarVisible == false) {
+        sidebar.style.display = "inline";
+    } else {
+        sidebar.style.display = "none";
+    }
+    isSidebarVisible = !isSidebarVisible;
 }
